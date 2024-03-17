d22a48ededda9215ec0fb7d2fe4bdf06b2848c3a
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-03-17 22:01:40 +0100

    Matching Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  {'src': '/img/helmet-1.png'},
-  {'src': '/img/potion-1.png'},
-  {'src': '/img/ring-1.png'},
-  {'src': '/img/scroll-1.png'},
-  {'src': '/img/shield-1.png'},
-  {'src': '/img/sword-1.png'},
+  {'src': '/img/helmet-1.png', matched: false},
+  {'src': '/img/potion-1.png',matched: false},
+  {'src': '/img/ring-1.png', matched: false},
+  {'src': '/img/scroll-1.png', matched: false},
+  {'src': '/img/shield-1.png', matched: false},
+  {'src': '/img/sword-1.png' ,matched: false},
 ]
 
 
@@ -17,7 +17,8 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
-
+console.log(cards);
+console.log(turns);
   // shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages] // Double array
@@ -36,8 +37,15 @@ function App() {
   useEffect(() => {
     if(choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src){
-        console.log('Picture Matched')
-        resetTurn()
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src ===choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
       } else{
         console.log('Picture Not Matched')
         resetTurn()
